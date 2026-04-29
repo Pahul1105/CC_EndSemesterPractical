05fa7f6a91fc7bf29c306508164ec484e9334e0b
Pahul1105 <11pahulsingh@gmail.com>
2026-04-29 16:11:54 +0530

    Update Question-3 Leetcode Longest Substring.cpp

--- Question-3 Leetcode Longest Substring.cpp
+++ Question-3 Leetcode Longest Substring.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> freq(128,0);
+        int left = 0, maxLen = 0;
+        for (int r=0;r<s.length();r++) {
+            freq[s[r]]++;
+            while (freq[s[r]]>1) {
+                freq[s[left]]--;
+                left++;
+            }
+            maxLen = max(maxLen,r-left+1);
+        }
+        return maxLen;
+    }
+};
+
 
